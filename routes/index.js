438670ac2561b604e03242dc681c128d875c8389
Raghav Tandon <raghav.tandon13@gmail.com>
2024-01-12 13:04:07 +0530

    git

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // send correct working response
   res.status(200).json({
     type: "success",
     message: "Server is up and running",
-  }); 
-  
+  });
 });
 
 module.exports = router;
