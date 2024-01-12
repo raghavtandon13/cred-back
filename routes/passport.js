0244cda9378aa5b6972a85f53b0b0ba417ee43fd
Raghav Tandon <raghav.tandon13@gmail.com>
2024-01-12 13:04:33 +0530

    git

--- routes/passport.js
+++ routes/passport.js
@@ -1,20 +1,23 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-  
-passport.serializeUser((user , done) => {
-    done(null , user);
-})
-passport.deserializeUser(function(user, done) {
-    done(null, user);
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
-  
-passport.use(new GoogleStrategy({
-    clientID:"1040252073430-ok2snj9f09kdmr1srip9csib8vuf6u1a.apps.googleusercontent.com", // Your Credentials here.
-    clientSecret:"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", // Your Credentials here.
-    callbackURL:"http://localhost:4000/auth/callback",
-    passReqToCallback:true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-));
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: "1040252073430-ok2snj9f09kdmr1srip9csib8vuf6u1a.apps.googleusercontent.com", // Your Credentials here.
+      clientSecret: "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", // Your Credentials here.
+      callbackURL: "http://localhost:4000/auth/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
