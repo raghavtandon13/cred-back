7da22d46f199e5c3ab4bffdcc1ea514c7930edb8
Raghav Tandon <raghav.tandon13@gmail.com>
2024-01-12 13:04:18 +0530

    git

--- routes/partner.js
+++ routes/partner.js
@@ -4,7 +4,6 @@ const checkAuth = require("../middlewares/checkAuth");
 const Partner = require("../models/partner.model");
 var router = express.Router();
 
-
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
@@ -23,14 +22,12 @@ router.post("/emi", (req, res) => {
   const { P, R, N } = req.body;
 
   if (!P || !R || !N) {
-    return res
-      .status(400)
-      .json({ message: "Please provide all three numbers" });
+    return res.status(400).json({ message: "Please provide all three numbers" });
   }
-  const iR = R / (12);
+  const iR = R / 12;
   console.log(iR);
-  const denominator = Math.pow(1 + iR*0.01, N) - 1;
-  const EMI = (P * iR * 0.01 * Math.pow(1 + iR*0.01, N)) / denominator;
+  const denominator = Math.pow(1 + iR * 0.01, N) - 1;
+  const EMI = (P * iR * 0.01 * Math.pow(1 + iR * 0.01, N)) / denominator;
 
   // const EMI = Number(P) + Number(R) + Number(N);
   res.json({ EMI });
@@ -72,7 +69,6 @@ router.get("/micro-loan", async (req, res) => {
   }
 });
 
-
 router.get("/pay-later", async (req, res) => {
   try {
     const partners = await Partner.find({ type: "payLater" });
@@ -87,14 +83,11 @@ router.get("/pay-later", async (req, res) => {
   }
 });
 
-
 router.get("/personal-loan", async (req, res) => {
   try {
     const partners = await Partner.find({ type: "personalLoan" });
     if (partners.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "Personal Loan Partners not found" });
+      return res.status(404).json({ message: "Personal Loan Partners not found" });
     }
 
     res.json(partners);
@@ -103,15 +96,12 @@ router.get("/personal-loan", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-
 
 router.get("/credit-card", async (req, res) => {
   try {
     const partners = await Partner.find({ type: "creditCard" });
     if (partners.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "Credit Card Partners not found" });
+      return res.status(404).json({ message: "Credit Card Partners not found" });
     }
 
     res.json(partners);
@@ -120,7 +110,6 @@ router.get("/credit-card", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-
 
 router.post("/", checkAuth, checkAdmin, async function (req, res, next) {
   try {
