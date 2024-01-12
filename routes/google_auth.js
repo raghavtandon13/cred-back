e46878efe5dd87dba54c046b2c8da294582c1eec
Raghav Tandon <raghav.tandon13@gmail.com>
2024-01-12 13:03:56 +0530

    git

--- routes/google_auth.js
+++ routes/google_auth.js
@@ -6,41 +6,29 @@ require("../passportConfig")(passport);
 
 const { JWT_SECRET } = require("../config");
 
-router.get(
-  "/",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
+router.get("/", passport.authenticate("google", { scope: ["email", "profile"] }));
 
-router.get(
-  "/callback",
+router.get("/callback", passport.authenticate("google", { session: false }), (req, res) => {
   passport.authenticate("google", { session: false }),
-  (req, res) => {
-    passport.authenticate("google", { session: false }),
-      (req, res) => {
-        jwt.sign(
-          { user: req.user },
-          JWT_SECRET,
-          { expiresIn: "24h" },
-          (err, token) => {
-            if (err) {
-              return res.json({
-                type: "error",
-                data: {
-                  token: null,
-                },
-              });
-            }
-            res.json({
-              type: "success",
-              data: {
-                token: token,
-                user: req.user,
-              },
-            });
-          }
-        );
-      };
-  }
-);
+    (req, res) => {
+      jwt.sign({ user: req.user }, JWT_SECRET, { expiresIn: "24h" }, (err, token) => {
+        if (err) {
+          return res.json({
+            type: "error",
+            data: {
+              token: null,
+            },
+          });
+        }
+        res.json({
+          type: "success",
+          data: {
+            token: token,
+            user: req.user,
+          },
+        });
+      });
+    };
+});
 
 module.exports = router;
